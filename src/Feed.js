e05c7711d003e9d0893f0875b6a9630a0aba58d9
CliftonOliver <clioli4453@students.ecpi.edu>
2024-08-30 03:27:58 -0400

    New commit

--- src/Feed.js
+++ src/Feed.js
@@ -1,29 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import "./Feed.css";
 import MessageSender from "./MessageSender";
 import Post from "./Post";
-import StoryReel from './StoryReel';
-import db from './firebase';
-
+import StoryReel from "./StoryReel";
+import db from "./firebase";
 
 function Feed() {
-  const[posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-  useEffect(() =>{
-    db.collection("posts").orderBy("timestamp", "desc")
-    .onSnapshot((snapshot) => 
-    setPosts(snapshot.docs.map((doc => ({ id: doc.id, data: doc.data() })))
-    ));
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })))
+      );
   }, []);
 
   return (
     <div className="feed">
-      <StoryReel />  
-      <MessageSender/>
+      <StoryReel />
+      <MessageSender />
 
       {posts.map((post) => (
         <Post
-          key ={post.data.id}
+          key={post.data.id}
           profilePic={post.data.profilePic}
           message={post.data.message}
           timestamp={post.data.timestamp}
